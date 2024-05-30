bdc299ab25612e12d258e9aad18910fd6f13a9c5
srinivle <50224645+srinivle@users.noreply.github.com>
2024-05-30 18:02:55 +0530

    new lines of code

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -26,9 +26,11 @@ locals {
     echo "Hello There!, Welcome to Green Environment"
     sudo yum update -y
     sudo yum install -y yum-utils
+    sudo yum install httpd -y
     sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo systemctl start docker
+    sudo systemctl restart httpd
     sudo systemctl restart httpd.service
     docker container run --name app -p 8080:8080 public.ecr.aws/u8w9f9i8/sample-app-updated
   EOT
